e164f05a10241255e686a7dfc7925fb977c80e75
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-07-16 21:50:18 +0700

    add local tunnel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      hello switch
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton />
     </div>
   );
 }
