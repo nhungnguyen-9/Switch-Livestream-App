ce8b6bd141a636fdb972e1db807b2d00c9db1416
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-07-18 10:31:08 +0700

    add sidebar

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Wrapper } from "./wrapper";
+import { Toggle } from "./toggle";
+
+export const Sidebar = () => {
+    // fetch followed
+
+    return (
+        <Wrapper>
+            <Toggle />
+        </Wrapper>
+    );
+};
