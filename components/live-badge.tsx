20d9d7e39d438dfed9719cba73cf52914ea5d571
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-07-19 15:27:15 +0700

    add recommended list

---
+++ components/live-badge.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface LiveBadgeProps {
+    className?: string
+}
+
+export const LiveBadge = ({
+    className
+}: LiveBadgeProps) => {
+    return (
+        <div className={cn(
+            'bg-rose-500 text-center p-0.5 px-1 rounded-[3px] uppercase text-[10px] font-bold tracking-wide',
+            className
+        )}>
+            live
+        </div>
+    );
+};
