a3cdcbc42474d8f57bdfb381d351e6b9c8fbeb08
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-08-12 21:55:30 +0700

    fix errors

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -13,9 +13,28 @@ import { Header, HeaderSkeleton } from "./header"
 import { InfoCard } from "./info-card"
 import { AboutCard } from "./about-card"
 
+type CustomStream = {
+    id: string,
+    isChatEnabled: boolean,
+    isChatDelayed: boolean,
+    isChatFollowersOnly: boolean,
+    isLive: boolean,
+    thumbnailUrl: string | null,
+    name: string
+}
+
+type CustomUser = {
+    id: string,
+    username: string,
+    bio: string | null,
+    stream: CustomStream | null,
+    imageUrl: string,
+    _count: { followedBy: number }
+}
+
 interface StreamLayerProps {
-    user: User & { stream: Stream | null, _count: { followedBy: number } },
-    stream: Stream,
+    user: CustomUser & { stream: CustomStream | null, _count: { followedBy: number } },
+    stream: CustomStream,
     isFollowing: boolean
 }
 
