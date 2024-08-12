67ab9e446e7bade0f8de0d9c2855809961626ded
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-08-12 22:05:26 +0700

    add search page

---
+++ app/(browse)/search/_components/results.tsx
@@ -0,0 +1,24 @@
+
+interface ResultsProps {
+    term?: string
+}
+
+export const Results = ({
+    term
+}: ResultsProps) => {
+    return (
+        <div>
+            <h2 className="text-lg font-semibold mb-4">
+                Results for term &quot;{term}&quot;
+            </h2>
+        </div>
+    )
+}
+
+export const ResultsSkeleton = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
